a5980f13d52dcb5f34bf4bcac5d9ff1e4a480c65
Lucas Yuji Tengan Ikeda <lucastengan@usp.br>
2020-07-14 21:55:26 -0300

    Backend

--- src/main.js
+++ src/main.js
@@ -5,86 +5,68 @@ class App {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
-        this.inputEl = document.querySelector('#repo-input');
+        this.inputEl = document.querySelector('input[name=repository]')
         this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
     }
 
     registerHandlers() {
-        this.formEl.onsubmit = (event) => this.addRepository(event);
-    }
-
-    setLoading(loading = true) {
-        if(loading === true) {
-            let loadingEl = document.createElement('span');
-            loadingEl.appendChild(document.createTextNode('Carregando...'))
-            loadingEl.setAttribute('id', 'loading');
-
-            this.formEl.appendChild(loadingEl);
-        } else {
-            document.getElementById('loading').remove();
-        }
+        this.formEl.onsubmit = (event) => this.addRepository(event)
     }
 
     async addRepository(event) {
         event.preventDefault();
-        
-        
+
         const repoInput = this.inputEl.value;
 
-        if(repoInput.length === 0)
+        if(repoInput.length === 0) 
             return;
+        
+        const response = await api.get(`/repos/${repoInput}`);
 
-        this.setLoading();
+        const { name, description, html_url, owner: { avatar_url} } = response.data;
 
-        try {
-            const response = await api.get(`/repos/${repoInput}`);
+        this.repositories.push({
+            name,
+            description,
+            avatar_url,
+            html_url
+        });
 
-            const { name, description, html_url, owner: {avatar_url}} = response.data;
+        this.inputEl = '';
 
-            this.repositories.push({
-                name,
-                description,
-                avatar_url,
-                html_url,
+        this.render();
+    }
+        render() {
+
+            this.listEl.innerHTML = '';
+
+            this.repositories.forEach(repo => {
+                let imgEl = document.createElement('img');
+                imgEl.setAttribute('src', repo.avatar_url);
+
+                let titleEl = document.createElement('strong');
+                titleEl.appendChild(document.createTextNode(repo.name));
+
+                let descriptionEl = document.createElement('p');
+                descriptionEl.appendChild(document.createTextNode(repo.description));
+
+                let linkEl = document.createElement('a');
+                linkEl.setAttribute('target', '_blank');
+                linkEl.setAttribute('href', repo.html_url)
+                linkEl.appendChild(document.createTextNode('Acessar'));
+
+                let listItemEl = document.createElement('li');
+                listItemEl.appendChild(imgEl);
+                listItemEl.appendChild(descriptionEl);
+                listItemEl.appendChild(titleEl);
+                listItemEl.appendChild(imgEl)
+
+                this.listEl.appendChild(listItemEl)
             });
-
-            this.inputEl.value = '';
-            this.render(); 
-        } catch(err) {
-            alert('O repositório não existe!');
         }
-        this.setLoading(false);
     }
 
-    render() {
-        this.listEl.innerHTML = '';
-
-        this.repositories.forEach(repo => {
-            let imgEl = document.createElement('img');
-            imgEl.setAttribute('src', repo.avatar_url);
-
-            let titleEl = document.createElement('strong');
-            titleEl.appendChild(document.createTextNode(repo.name));
-
-            let descriptionEl = document.createElement('p');
-            descriptionEl.appendChild(document.createTextNode(repo.description));
-
-            let linkEl = document.createElement('a');
-            linkEl.setAttribute('target', '_blank');
-            linkEl.setAttribute('href', repo.html_url);
-            linkEl.appendChild(document.createTextNode('Acessar'));
-
-            let listItemEl = document.createElement('li');
-            listItemEl.appendChild(imgEl);
-            listItemEl.appendChild(titleEl);
-            listItemEl.appendChild(descriptionEl);
-            listItemEl.appendChild(linkEl);
-
-            this.listEl.appendChild(listItemEl);
-        })
-    }
-}
 
 new App();
